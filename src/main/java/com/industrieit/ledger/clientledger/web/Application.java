11ddeb1ce853c3c069bedbefd7f3bf7b2c14ef46
bit02 <113297383+bit02@users.noreply.github.com>
2021-07-24 08:27:42 +0000

    see last commit

--- src/main/java/com/industrieit/ledger/clientledger/web/Application.java
+++ src/main/java/com/industrieit/ledger/clientledger/web/Application.java
@@ -2,4 +2,5 @@ package com.industrieit.ledger.clientledger.web;
 
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.Exit
+import org.springframework.boot.ExitCodeGenerator;
+import org.
