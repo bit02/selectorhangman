c781605cd33a6f89e6c2982799f7eda775034b29
bit02 <113297383+bit02@users.noreply.github.com>
2021-07-25 20:12:57 +0000

    new package

--- src/main/java/com/industrieit/ledger/clientledger/web/Application.java
+++ src/main/java/com/industrieit/ledger/clientledger/web/Application.java
@@ -4,4 +4,4 @@ package com.industrieit.ledger.clientledger.web;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
-import org.spring
+import org.springframework.boot.autoconfigure.
