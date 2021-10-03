2d8923bc28c7e594e9593e232b583caf64e16288
bit02 <113297383+bit02@users.noreply.github.com>
2021-10-03 19:38:25 +0000

    Update daemon.py

--- src/main/java/com/industrieit/ledger/clientledger/web/controller/TransactionController.java
+++ src/main/java/com/industrieit/ledger/clientledger/web/controller/TransactionController.java
@@ -4,4 +4,5 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.industrieit.ledger.clientledger.web.entity.TransactionEvent;
 import com.industrieit.ledger.clientledger.web.exception.LedgerServiceErrorMessage;
-import com.industrieit.ledger.clientledger.web.exception.Service
+import com.industrieit.ledger.clientledger.web.exception.ServiceException;
+import com.
