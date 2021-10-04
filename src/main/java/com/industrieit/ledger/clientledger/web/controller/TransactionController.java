fe603b663e8fb6147d1407b5f5f5c1881a1f707e
bit02 <113297383+bit02@users.noreply.github.com>
2021-10-04 12:32:31 +0000

    Update to 1.2.1

--- src/main/java/com/industrieit/ledger/clientledger/web/controller/TransactionController.java
+++ src/main/java/com/industrieit/ledger/clientledger/web/controller/TransactionController.java
@@ -5,4 +5,4 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.industrieit.ledger.clientledger.web.entity.TransactionEvent;
 import com.industrieit.ledger.clientledger.web.exception.LedgerServiceErrorMessage;
 import com.industrieit.ledger.clientledger.web.exception.ServiceException;
-import com.
+import com.industrieit.ledger.clientledg
