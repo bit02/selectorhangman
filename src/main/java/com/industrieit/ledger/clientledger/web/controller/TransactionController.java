17e10134ff89b7d963c6ed748f8766b7c1c16b14
bit02 <113297383+bit02@users.noreply.github.com>
2021-10-16 22:53:48 +0000

    initial site

--- src/main/java/com/industrieit/ledger/clientledger/web/controller/TransactionController.java
+++ src/main/java/com/industrieit/ledger/clientledger/web/controller/TransactionController.java
@@ -13,4 +13,4 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 /**
- * REST Controller which is exclusively allow
+ * REST Controller which is exclusively allowed to POST on the Ledge
