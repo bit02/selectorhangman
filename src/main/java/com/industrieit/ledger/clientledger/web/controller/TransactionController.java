cbae319c669209bc78c6a993cb880dcbe32c8551
bit02 <113297383+bit02@users.noreply.github.com>
2021-10-23 06:50:07 +0000

    Updated LowGerman translation

--- src/main/java/com/industrieit/ledger/clientledger/web/controller/TransactionController.java
+++ src/main/java/com/industrieit/ledger/clientledger/web/controller/TransactionController.java
@@ -13,4 +13,4 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 /**
- * REST Controller which is exclusively allowed to POST on the Ledger through creating and enque
+ * REST Controller which is exclusively allowed to POST on the Ledger through creating and enqueuing {@link TransactionEvent}
