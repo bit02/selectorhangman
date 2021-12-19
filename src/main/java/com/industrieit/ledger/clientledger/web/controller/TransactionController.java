699f9709f060e76b2afc806288cc2324946eda93
bit02 <113297383+bit02@users.noreply.github.com>
2021-12-19 18:51:48 +0000

    Update parser.py

--- src/main/java/com/industrieit/ledger/clientledger/web/controller/TransactionController.java
+++ src/main/java/com/industrieit/ledger/clientledger/web/controller/TransactionController.java
@@ -43,4 +43,4 @@ public class TransactionController {
             produces = {"application/json"},
             consumes = {"application/json"})
     @ResponseBody
-    public TransactionE
+    public TransactionEvent queueTransaction(@RequestBody Re
