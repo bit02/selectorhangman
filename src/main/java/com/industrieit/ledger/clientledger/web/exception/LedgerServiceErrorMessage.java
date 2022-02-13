f2c22a4027868f74e415c44d8aa794ab02022bfd
bit02 <113297383+bit02@users.noreply.github.com>
2022-02-13 17:07:12 +0000

    Initial commit :tada:

--- src/main/java/com/industrieit/ledger/clientledger/web/exception/LedgerServiceErrorMessage.java
+++ src/main/java/com/industrieit/ledger/clientledger/web/exception/LedgerServiceErrorMessage.java
@@ -1,4 +1,5 @@
 package com.industrieit.ledger.clientledger.web.exception;
 
 
-import com.industrieit.ledger.clientledger.web.entity.Transaction
+import com.industrieit.ledger.clientledger.web.entity.TransactionEvent;
+import com.industrieit.ledger.
