6a51a9b05dc79e2cd5265015b22b7f4a946eaf4c
bit02 <113297383+bit02@users.noreply.github.com>
2022-02-18 13:30:16 +0000

    Update addons2.ini

--- src/main/java/com/industrieit/ledger/clientledger/web/exception/LedgerServiceErrorMessage.java
+++ src/main/java/com/industrieit/ledger/clientledger/web/exception/LedgerServiceErrorMessage.java
@@ -5,4 +5,5 @@ import com.industrieit.ledger.clientledger.web.entity.TransactionEvent;
 import com.industrieit.ledger.clientledger.web.model.request.RequestEnvelop;
 
 /**
- * Runtime transaction thrown before {@link Reque
+ * Runtime transaction thrown before {@link RequestEnvelop} is accepted
+ * No {@link Tr
