db8c33e974cd102964e2caff6a494814eb53c7c3
bit02 <113297383+bit02@users.noreply.github.com>
2022-03-15 20:33:38 +0000

    Control commit

--- src/main/java/com/industrieit/ledger/clientledger/web/exception/LedgerServiceErrorMessage.java
+++ src/main/java/com/industrieit/ledger/clientledger/web/exception/LedgerServiceErrorMessage.java
@@ -9,4 +9,5 @@ import com.industrieit.ledger.clientledger.web.model.request.RequestEnvelop;
  * No {@link TransactionEvent} will be created and enqueued when the exception is thrown
  */
 public enum LedgerServiceErrorMessage implements MessageDetail {
-    TYPE_NOT_SUPPORTED("LDR-1001", "Transaction Type Not Supported")
+    TYPE_NOT_SUPPORTED("LDR-1001", "Transaction Type Not Supported"),
+    REQUEST_UNREADABLE("LDR-100
