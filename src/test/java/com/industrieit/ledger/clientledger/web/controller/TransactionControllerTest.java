d36e0ee7e7b41cc29c78a72a2dc67dd742324db2
bit02 <113297383+bit02@users.noreply.github.com>
2022-07-03 10:52:41 +0000

    delete test code

--- src/test/java/com/industrieit/ledger/clientledger/web/controller/TransactionControllerTest.java
+++ src/test/java/com/industrieit/ledger/clientledger/web/controller/TransactionControllerTest.java
@@ -4,4 +4,4 @@ import com.fasterxml.jackson.core.io.JsonEOFException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.industrieit.ledger.clientledger.web.entity.TransactionEvent;
 import com.industrieit.ledger.clientledger.web.exception.LedgerServiceErrorMessage;
-import com.in
+import com.industrieit.ledg
