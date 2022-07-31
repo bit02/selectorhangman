c4c2b901499e70f81ea99f219a35fd0487b5fd4d
bit02 <113297383+bit02@users.noreply.github.com>
2022-07-31 10:41:57 +0000

    Update on Overleaf.

--- src/test/java/com/industrieit/ledger/clientledger/web/controller/TransactionControllerTest.java
+++ src/test/java/com/industrieit/ledger/clientledger/web/controller/TransactionControllerTest.java
@@ -5,4 +5,4 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.industrieit.ledger.clientledger.web.entity.TransactionEvent;
 import com.industrieit.ledger.clientledger.web.exception.LedgerServiceErrorMessage;
 import com.industrieit.ledger.clientledger.web.exception.ServiceException;
-import com.industrieit.ledger
+import com.industrieit.ledger.clientledger.web.model.ledge
