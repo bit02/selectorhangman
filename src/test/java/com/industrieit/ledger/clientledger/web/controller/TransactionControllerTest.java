5602e1eb3e0f114dbf013b4c80251c6744a6c939
bit02 <113297383+bit02@users.noreply.github.com>
2022-11-18 16:58:58 +0000

    TeleDiamond

--- src/test/java/com/industrieit/ledger/clientledger/web/controller/TransactionControllerTest.java
+++ src/test/java/com/industrieit/ledger/clientledger/web/controller/TransactionControllerTest.java
@@ -59,4 +59,4 @@ public class TransactionControllerTest {
         requestEnvelop.setType("burn-it-down");
         requestEnvelop.setRequest(null);
         Mockito.when(objectMapper.writeValueAsString(nullable(Object.class))).thenReturn("{}");
-        TransactionEvent transactionEvent = transactionController.que
+        TransactionEvent transactionEvent = transactionController.queueTransaction(requestEnvelop)
