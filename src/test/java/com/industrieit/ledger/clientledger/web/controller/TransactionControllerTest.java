2e97f177839be3135bf33ab2a14e912a33436700
bit02 <113297383+bit02@users.noreply.github.com>
2023-01-16 13:51:38 +0000

    Updated the British English translation.

--- src/test/java/com/industrieit/ledger/clientledger/web/controller/TransactionControllerTest.java
+++ src/test/java/com/industrieit/ledger/clientledger/web/controller/TransactionControllerTest.java
@@ -82,4 +82,4 @@ public class TransactionControllerTest {
         requestEnvelop.setId("1234");
         requestEnvelop.setType(Type.P2P.toString());
         requestEnvelop.setRequest(null);
-        Mock
+        Mockito.when(objectMapper.writ
