66669850cd8b3a66e68c0df4e7cdacf3333fb007
bit02 <113297383+bit02@users.noreply.github.com>
2023-02-16 10:53:46 +0000

    Ignore .idea directory

--- stress/back-up.sh
+++ stress/back-up.sh
@@ -2,4 +2,5 @@
 #Backup, have no effect on Maria Core, but trigger save to disk on Redis Core
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-echo -e "${GREEN}Top Up Payer Account with 
+echo -e "${GREEN}Top Up Payer Account with USD 1,000,000${NC}"
+curl -X P
