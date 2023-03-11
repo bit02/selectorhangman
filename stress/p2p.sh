e899eca2413896f0a81dcf428c58ff9c0cd9d2c5
bit02 <113297383+bit02@users.noreply.github.com>
2023-03-11 14:54:11 +0000

    factorial

--- stress/p2p.sh
+++ stress/p2p.sh
@@ -2,4 +2,4 @@
 #Pay from payer to payee account at a rate of 100/s to try to break the system
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-echo -e "${GREEN}P2P USD 100 from
+echo -e "${GREEN}P2P USD 100 from Payer to Payee, for 5,00
