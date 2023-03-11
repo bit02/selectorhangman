3f25bfe6a2e7ac0264abf87a658ccf636e8b53ee
bit02 <113297383+bit02@users.noreply.github.com>
2023-03-11 14:38:31 +0000

    Config files

--- stress/p2p.sh
+++ stress/p2p.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 #Pay from payer to payee account at a rate of 100/s to try to break the system
 GREEN='\033[0;32m'
-NC='\033[0m' # No Co
+NC='\033[0m' # No Color
+echo -e "${GREEN}P2P USD 100 from
