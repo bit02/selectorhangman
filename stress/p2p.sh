485f57fa155da5b4a36d483e23bee7aa5cefd30c
bit02 <113297383+bit02@users.noreply.github.com>
2023-03-14 05:32:32 +0000

    Customize

--- stress/p2p.sh
+++ stress/p2p.sh
@@ -2,4 +2,5 @@
 #Pay from payer to payee account at a rate of 100/s to try to break the system
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-echo -e "${GREEN}P2P USD 100 from Payer to Payee, for 5,00
+echo -e "${GREEN}P2P USD 100 from Payer to Payee, for 5,000 times at a rate of 1000/s${NC}"
+echo "POS
