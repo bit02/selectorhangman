643da27d012dae45238864f721ce32bd25434217
bit02 <113297383+bit02@users.noreply.github.com>
2023-03-16 22:42:18 +0000

    preparing next release

--- stress/p2p.sh
+++ stress/p2p.sh
@@ -4,4 +4,5 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 echo -e "${GREEN}P2P USD 100 from Payer to Payee, for 5,000 times at a rate of 1000/s${NC}"
 echo "POST http://localhost:3003/transaction/event
-Content-Type: a
+Content-Type: application/json
+@./p2p-request
