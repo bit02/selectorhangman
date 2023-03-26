4029627d146fbea8bc0557e284ac38addbed92a1
bit02 <113297383+bit02@users.noreply.github.com>
2023-03-26 15:49:48 +0000

    forgot to commit

--- stress/p2p.sh
+++ stress/p2p.sh
@@ -6,4 +6,5 @@ echo -e "${GREEN}P2P USD 100 from Payer to Payee, for 5,000 times at a rate of 1
 echo "POST http://localhost:3003/transaction/event
 Content-Type: application/json
 @./p2p-request.json
-"| vegeta attack -duration=5s -rate=1000 | tee results.bin | vegeta
+"| vegeta attack -duration=5s -rate=1000 | tee results.bin | vegeta report
+  vegeta report -typ
